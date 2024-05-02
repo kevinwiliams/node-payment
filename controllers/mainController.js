6377bd395f23cf0c57e79cae93af8908c3af4ea9
Kevin Williams <kwilliams9285@gmail.com>
2024-05-01 23:56:56 -0500

    Update main Add checkout

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -23,7 +23,7 @@ async function getMain(req, res){
     try {
         const { id } = req.body;
         console.log('id', req.body);
-        const services = await Service.findAll({ where: { categoryId : parseInt(id)}, order:[ ['name', 'ASC'] ] });
+        const services = await Service.findAll({ where: { categoryId : parseInt(id)}, order:[ ['price', 'DESC'] ] });
         // return services;
         res.json({
             services: services
@@ -35,7 +35,33 @@ async function getMain(req, res){
     }
   }
 
+  async function loadPaymentInfo(req, res) {
+    console.log('paymentInfo', req.body);
+    const { category, service, serviceName, serviceText, categoryName, categoryId, pricehd, currency, price, otherInfo, description } = req.body;
+
+    // Assuming there's only one category and service selected
+    const selectedPrice = (price[0]) ? price[0] : pricehd[0];
+    const selectedCurrency = currency[0];
+    const selectedOtherInfo = otherInfo[0];
+
+    const paymentInfo = {
+        categoryName: categoryName,
+        categoryId: category,
+        serviceName: serviceText,
+        description: description,
+        serviceId: service,
+        price: selectedPrice,
+        currency: selectedCurrency,
+        otherInfo: selectedOtherInfo
+    };
+
+    // Optionally, you can send a response back
+    res.json({ success: paymentInfo });
+    
+  }
+
   module.exports = {
     getMain,
-    getService
+    getService,
+    loadPaymentInfo
   };
--- routes/en.js
+++ routes/en.js
@@ -4,6 +4,7 @@ const mainController = require('../controllers/mainController');
 const paymentController = require('../controllers/paymentController');
 
 router.get('/', mainController.getMain);
+router.post('/checkout', mainController.loadPaymentInfo);
 router.post('/service', mainController.getService);
 router.post('/auth', paymentController.authenticate);
 router.post('/confirmation', paymentController.completePayment);
